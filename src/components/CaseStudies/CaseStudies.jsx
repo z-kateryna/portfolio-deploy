7cf126ffaa364a9cb4470f97b28934d36f807f86
Your Name <you@example.com>
2025-05-14 15:21:55 -0700

    latest changes

--- src/components/CaseStudies/CaseStudies.jsx
+++ src/components/CaseStudies/CaseStudies.jsx
@@ -7,8 +7,8 @@ import COBMockUp from "../../assets/images/cob-mock-up.jpg";
 
 export default function CaseStudies() {
   return (
-    <>
-      <section className="case-studies">
+    <section className="case-studies">
+      <div className="case-studies__wrapper">
         <a className="case-studies__pyrukynd" href="#">
           <img className="case-studies__pyrukynd--img" src={PurykundMockUp} />
           <div className="case-studies__tag-container">
@@ -17,8 +17,16 @@ export default function CaseStudies() {
             <div className="case-studies__tag">Visual Design</div>
           </div>
           <div className="case-studies__copy-container">
-            <h2 className="case-studies__header">Designing for Impact: Launching Pyrukynd's Digital Presence</h2>
-            <p className="case-studies__copy">This case study explores the design and development process behind the Pyrukynd website, focusing on creating an engaging, user-friendly digital resource for patients and healthcare providers, aimed at delivering crucial treatment information with clarity and ease.</p>
+            <h2 className="case-studies__header">
+              Designing for Impact: Launching Pyrukynd's Digital Presence
+            </h2>
+            <p className="case-studies__copy">
+              This case study explores the design and development process behind
+              the Pyrukynd website, focusing on creating an engaging,
+              user-friendly digital resource for patients and healthcare
+              providers, aimed at delivering crucial treatment information with
+              clarity and ease.
+            </p>
             {/* <button className="case-studies__btn">
               read case study
               <img src={Arrow} />
@@ -73,7 +81,7 @@ export default function CaseStudies() {
             </button> */}
           </div>
         </a>
-      </section>
-    </>
+      </div>
+    </section>
   );
 }
